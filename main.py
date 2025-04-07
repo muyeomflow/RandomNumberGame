c5f2c6deedbbb8e08ee06e17ca7acc15d49a50e4
muyeomflow <muyeom100sh@gmail.com>
2025-04-07 13:07:12 +0900

    변경사항

--- main.py
+++ main.py
@@ -1,50 +1,58 @@
 import random
 
 answer = random.randint(1,1000)
-
-guess = 0
+user_input = 0
 attempts = 0
             
 while True:
     
     if attempts <= 20: #Up to 20 times
-    
-        try:
-                
-            guess = int(input("Enter a number between 1 and 1000 : "))
-            attempts += 1 #Increasing the number of attempts
+ 
+            user_input = input("Enter a number between 1 and 1000 : ")
             
-            if guess == answer:
-                
-                print(f"correct answer! You guessed it in {attempts} attempts.")
-                break
+            if user_input == int:
             
-            elif guess > answer:
+                attempts += 1 #Increasing the number of attempts
                 
-                if guess > 1000:
+                if user_input == answer:
                     
-                    print(f"Out of range of numbers. Please re-enter. You guessed it in {attempts} attempts.")
+                    print(f"correct answer! You guessed it in {attempts} attempts.")
+                    break
+                
+                elif user_input > answer:
                     
+                    if user_input > 1000:
+                        
+                        print(f"Out of range of numbers. Please re-enter. You guessed it in {attempts} attempts.")
+                        
+                    else:
+                        
+                        print(f"Down! Number of attempts : {attempts} attempts.")
+                    
+                elif user_input < answer:
+                    
+                    if user_input < 1:
+                        
+                        print(f"Out of range of numbers. Please re-enter. You guessed it in {attempts} attempts.")
+                        
+                    else:
+                        
+                        print(f"Up! Number of attempts : {attempts} attempts.")
+            
+            elif user_input == str:
+                
+                if user_input == "break":
+                    
+                    break
+                
                 else:
                     
-                    print(f"Down! Number of attempts : {attempts} attempts.")
+                    print(f"It's not a number. Please re-enter. You guessed it in {attempts} attempts.")
                 
-            elif guess < answer:
+            elif user_input == float:
                 
-                if guess < 1:
-                    
-                    print(f"Out of range of numbers. Please re-enter. You guessed it in {attempts} attempts.")
-                    
-                else:
-                    
-                    print(f"Up! Number of attempts : {attempts} attempts.")
+                print(f"Please enter an integer. You guessed it in {attempts} attempts.")
 
-        except ValueError: 
-            
-            attempts += 1
-            print(f"Invalid input, please enter a number. You guessed it in {attempts} attempts.")
-            continue
-            
     else:
         
         print("You've exceeded 20 attempts. Game over!")
